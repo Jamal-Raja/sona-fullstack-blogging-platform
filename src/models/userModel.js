b9555b8aecb32acd5b76e1913922a1d6c1d64965
Jamal <jamalraja083@gmail.com>
2025-11-12 18:42:18 +0000

    Refactor user model to define attributes and validation rules

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,52 @@
-// const { Model, DataTypes } = require("sequelize");
-// const sequelize = require("../config/connection");
-// const bcrypt = require("bcryptjs");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// class User extends Model {
-//   checkPassword(loginPw) {
-//     return bcrypt.compareSync(loginPw, this.password);
-//   }
-// }
+const User = sequelize.define("User", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: "Name is required",
+      },
+      len: {
+        args: [1, 69],
+        msg: "Please ensure name is between 1 and 70 characters long.",
+      },
+    },
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: {
+        msg: "Email field is required",
+      },
+      isEmail: {
+        msg: "Please enter a valid email.",
+      },
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: "Please enter a password.",
+      },
+      len: {
+        args: [8],
+        msg: "Password must be at least 8 characters long.",
+      },
+    },
+  },
+});
+
+module.exports = User;
