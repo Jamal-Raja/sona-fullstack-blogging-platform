eb157c7bd54edd240bc42ea69bd8121aa7e968cc
Jamal <jamalraja083@gmail.com>
2025-11-16 20:15:12 +0000

    feat(routes): protect blog creation, deletion, and update routes with authentication and ownership verification

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blogController");
+const { authenticateToken } = require("../middleware/authenticateToken");
+const { verifyOwnership } = require("../middleware/verifyOwnership");
 
 // === BLOG ROUTES ===
 router
   .route("/")
   .get(blogController.fetchAllBlogs) // get all blogs with optional filtering
-  .post(blogController.createNewBlog); // create new blog
+  .post(authenticateToken, verifyOwnership, blogController.createNewBlog); // create new blog (Protected route - requires bearer valid bearer token)
 
 router
   .route("/:id") // get, delete or update single blog by id
   .get(blogController.fetchSingleBlog)
-  .delete(blogController.deleteBlog)
-  .patch(blogController.updateBlog);
+  .delete(authenticateToken, verifyOwnership, blogController.deleteBlog)
+  .patch(authenticateToken, verifyOwnership, blogController.updateBlog);
 
 module.exports = router;
